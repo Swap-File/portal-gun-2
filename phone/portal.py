71d33cf695a518e2a54f817c231a3c724d00e91e
swap-file <bigmarkslp@gmail.com>
2016-03-15 13:23:29 -0500

    PHP Backend Done

--- phone/portal.py
+++ phone/portal.py
@@ -1,5 +1,6 @@
 import pprint
 import time
+from datetime import datetime
 import requests
 
 auth = ('', '')
@@ -9,48 +10,81 @@ url_chell_data ='http://192.168.1.23/tmp/portal.txt'
 url_goron_image = 'http://192.168.1.22/tmp/snapshot.jpg'
 url_chell_image = 'http://192.168.1.23/tmp/snapshot.jpg'
 
+gordon_packet_id_last = -1
+chell_packet_id_last = -1
+gordon_last_seen_time = -1
+chell_last_seen_time = -1
+
 session_gordon = requests.Session()
 session_chell = requests.Session()
 session_upload = requests.Session()
 
-#get file from gun 1
-gordon_text = session_gordon.get(url_goron_data).content.split( )
-gordon = {};
-gordon['state_solo'] = int(gordon_text[0])
-gordon['state_duo'] = int(gordon_text[1])
-gordon['connected'] = bool(gordon_text[2])
-gordon['battery_level'] = float(gordon_text[26])
-gordon['temp'] = float(gordon_text[27])
-gordon['core_temp'] = float(gordon_text[28])
-gordon['latency'] = float(gordon_text[29])
-gordon['packet_counter'] = int(gordon_text[30])
+cycle = 0
+while (cycle < 2):
+	payload = {};
 
-#get file from gun 2
-chell_text = session_chell.get(url_chell_data).content.split( )
-chell = {};
-chell['state_solo'] = int(chell_text[0])
-chell['state_duo'] = int(chell_text[1])
-chell['connected'] = bool(chell_text[2])
-chell['battery_level'] = float(chell_text[26])
-chell['temp'] = float(chell_text[27])
-chell['core_temp'] = float(chell_text[28])
-chell['latency'] = float(chell_text[29])
-chell['packet_counter'] = int(chell_text[30])
+	#get file from gun 1
+	gordon_text = session_gordon.get(url_goron_data).content.split( )
 
-#determine which gun if any to get image from
-pprint.pprint(chell)
-pprint.pprint(gordon)
+	gordon_packet_counter = int(gordon_text[30])
+	if (gordon_packet_counter != gordon_packet_id_last and gordon_packet_id_last != -1):
+		payload['g_state'] = int(gordon_text[1])
+		gordon_syned = int(gordon_text[2])
+		payload['g_volts'] = float(gordon_text[26])
+		payload['g_temp1'] = float(gordon_text[27])
+		payload['g_temp2'] = float(gordon_text[28])
+		payload['g_lag'] = float(gordon_text[29])
+		payload['g_live'] = 1
+	else:
+		payload['g_state'] = 0
+		gordon_syned = 0
+		payload['g_volts'] = 0
+		payload['g_temp1'] = 0
+		payload['g_temp2'] = 0
+		payload['g_lag'] = 0
+		payload['g_live'] = 0
+	gordon_packet_id_last = gordon_packet_counter	
 
-payload = {};
+	#get file from gun 2
+	chell_text = session_chell.get(url_chell_data).content.split( )
+	
+	chell_packet_counter = int(chell_text[30])
+	if (chell_packet_counter != chell_packet_id_last and chell_packet_id_last != -1):
+		payload['c_state'] = int(chell_text[1])
+		chell_synced = int(chell_text[2])
+		payload['c_volts'] = float(chell_text[26])
+		payload['c_temp1'] = float(chell_text[27])
+		payload['c_temp2'] = float(chell_text[28])
+		payload['c_lag'] = float(chell_text[29])
+		payload['c_live'] = 1
+	else:
+		payload['c_state'] = 0
+		chell_synced = 0
+		payload['c_volts'] = 0
+		payload['c_temp1'] = 0
+		payload['c_temp2'] = 0
+		payload['c_lag'] = 0
+		payload['c_live'] = 0
+	chell_packet_id_last = chell_packet_counter
 
-if chell['state_duo'] < 0:
-	payload['img'] = ('i.jpg', session_chell.get(url_chell_image).content)
-if gordon['state_duo'] < 0:
-	payload['img'] = ('i.jpg', session_gordon.get(url_goron_image).content)
-
-#post data to server
-payload['csv'] = ('r.csv', 'some,data,to,send')
-
-upload_result = session_upload.post(url_upload, auth=auth,files=payload)
-upload_result =  str( upload_result.content, encoding=upload_result.encoding ) 
-print (upload_result)
+	if (chell_synced == 1 and gordon_syned == 1):
+		payload['synced'] = 1
+	else:
+		payload['synced'] = 0
+		
+	#determine which gun if any to get image from
+	files = {};
+	if payload['c_state'] < 0:
+		files['img'] = ('i.jpg', session_chell.get(url_chell_image).content)
+			
+	if payload['g_state'] < 0:
+		files['img'] = ('i.jpg', session_gordon.get(url_goron_image).content)
+		
+	#supress warmup cycle
+	if cycle > 0:
+		upload_result = session_upload.post(url_upload,data=payload, auth=auth,files=files)
+		upload_result = str( upload_result.content, encoding=upload_result.encoding ) 
+		print (upload_result)
+		
+	cycle += 1
+	time.sleep(2)
--- website/https/add.php
+++ website/https/add.php
@@ -5,27 +5,37 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    $stmt = $conn->prepare("INSERT INTO log (image,ip) VALUES (:image,:ip)");
+    $stmt = $conn->prepare("INSERT INTO log (img,ip,g_state,c_state,g_lag,c_lag,g_temp1,c_temp1,g_temp2,c_temp2,g_volts,c_volts,g_live,c_live,synced)
+	VALUES (:img,:ip,:g_state,:c_state,:g_lag,:c_lag,:g_temp1,:c_temp1,:g_temp2,:c_temp2,:g_volts,:c_volts,:g_live,:c_live,:synced)");
 
-	var_dump( $_SERVER['REMOTE_ADDR']) ;
-	var_dump( $_FILES["img"]) ;
-	
 	$imageData = file_get_contents($_FILES["img"]["tmp_name"]);
-	$textData = explode(",",file_get_contents($_FILES["csv"]["tmp_name"]));
-	
-	var_dump($textData);
-	
-	
-    $stmt->bindParam(':image', $imageData, PDO::PARAM_LOB);
-	
+
+    $stmt->bindParam(':img', $imageData, PDO::PARAM_LOB);
 	$stmt->bindParam(':ip', $_SERVER['REMOTE_ADDR'], PDO::PARAM_STR);
 	
+	$stmt->bindParam(':g_state', $_POST["g_state"], PDO::PARAM_INT);
+	$stmt->bindParam(':c_state', $_POST["c_state"], PDO::PARAM_INT);
+	
+	$stmt->bindParam(':g_lag', $_POST["g_lag"], PDO::PARAM_STR);
+	$stmt->bindParam(':c_lag', $_POST["c_lag"], PDO::PARAM_STR);
+
+	$stmt->bindParam(':g_temp1', $_POST["g_temp1"], PDO::PARAM_STR);
+	$stmt->bindParam(':c_temp1', $_POST["c_temp1"], PDO::PARAM_STR);
+
+	$stmt->bindParam(':g_temp2', $_POST["g_temp2"], PDO::PARAM_STR);
+	$stmt->bindParam(':c_temp2', $_POST["c_temp2"], PDO::PARAM_STR);
+	
+	$stmt->bindParam(':g_volts', $_POST["g_volts"], PDO::PARAM_STR);
+	$stmt->bindParam(':c_volts', $_POST["c_volts"], PDO::PARAM_STR);
+	
+	$stmt->bindParam(':g_live', $_POST["g_live"], PDO::PARAM_INT);
+	$stmt->bindParam(':c_live', $_POST["c_live"], PDO::PARAM_INT);
+	
+	$stmt->bindParam(':synced', $_POST["synced"], PDO::PARAM_INT);
 
     $stmt->execute();
 			
-			
-
-    echo 'done' ;
+    echo 'done';
 }
 catch (PDOException $e) {
     print "Error: " . $e->getMessage();
