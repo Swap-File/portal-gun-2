d53e9aceaa8857d28847c82f2c355b7e02f04da9
swap-file <bigmarkslp@gmail.com>
2016-04-18 20:45:47 -0500

    Bluetooth

--- phone/portal.py
+++ phone/portal.py
@@ -61,24 +61,38 @@ def upload_data(keyframe):
 	global gordon_last_seen_time
 	global chell_last_seen_time
 
-	payload = {}
 	
+	payload = {}
 	payload['keyframe'] = keyframe
 	
-	try:
-		#get file from gun 1
-		gordon_text = session_gordon.get(url_goron_data).content.split( )
+	retries = 0
+	while retries < 2:
+		try:
+			retries = retries + 1
+			#get file from gun 1
+			gordon_text = session_gordon.get(url_goron_data,timeout=0.01).content.split( )
 
-		gordon_packet_counter = int(gordon_text[30])
-		if (gordon_packet_counter != gordon_packet_id_last and gordon_packet_id_last != -1):
-			payload['g_state'] = int(gordon_text[1])
-			gordon_syned = int(gordon_text[2])
-			payload['g_volts'] = float(gordon_text[26])
-			payload['g_temp1'] = float(gordon_text[27])
-			payload['g_temp2'] = float(gordon_text[28])
-			payload['g_lag'] = float(gordon_text[29])
-			payload['g_live'] = 1
-		else:
+			gordon_packet_counter = int(gordon_text[30])
+			if (gordon_packet_counter != gordon_packet_id_last and gordon_packet_id_last != -1):
+				payload['g_state'] = int(gordon_text[1])
+				gordon_syned = int(gordon_text[2])
+				payload['g_volts'] = float(gordon_text[26])
+				payload['g_temp1'] = float(gordon_text[27])
+				payload['g_temp2'] = float(gordon_text[28])
+				payload['g_lag'] = float(gordon_text[29])
+				payload['g_live'] = 1
+			else:
+				payload['g_state'] = 0
+				gordon_syned = 0
+				payload['g_volts'] = 0
+				payload['g_temp1'] = 0
+				payload['g_temp2'] = 0
+				payload['g_lag'] = 0
+				payload['g_live'] = 0
+			gordon_packet_id_last = gordon_packet_counter	
+			retries = 99;
+		except:
+			print ("Gordon Failed")
 			payload['g_state'] = 0
 			gordon_syned = 0
 			payload['g_volts'] = 0
@@ -86,31 +100,35 @@ def upload_data(keyframe):
 			payload['g_temp2'] = 0
 			payload['g_lag'] = 0
 			payload['g_live'] = 0
-		gordon_packet_id_last = gordon_packet_counter	
-	except:
-		print ("Gordon Failed")
-		payload['g_state'] = 0
-		gordon_syned = 0
-		payload['g_volts'] = 0
-		payload['g_temp1'] = 0
-		payload['g_temp2'] = 0
-		payload['g_lag'] = 0
-		payload['g_live'] = 0
 		
-	try:	
-		#get file from gun 2
-		chell_text = session_chell.get(url_chell_data).content.split( )
-		
-		chell_packet_counter = int(chell_text[30])
-		if (chell_packet_counter != chell_packet_id_last and chell_packet_id_last != -1):
-			payload['c_state'] = int(chell_text[1])
-			chell_synced = int(chell_text[2])
-			payload['c_volts'] = float(chell_text[26])
-			payload['c_temp1'] = float(chell_text[27])
-			payload['c_temp2'] = float(chell_text[28])
-			payload['c_lag'] = float(chell_text[29])
-			payload['c_live'] = 1
-		else:
+	retries = 0
+	while retries < 2:
+		try:
+			retries = retries + 1	
+			#get file from gun 2
+			chell_text = session_chell.get(url_chell_data,timeout=0.01).content.split( )
+			
+			chell_packet_counter = int(chell_text[30])
+			if (chell_packet_counter != chell_packet_id_last and chell_packet_id_last != -1):
+				payload['c_state'] = int(chell_text[1])
+				chell_synced = int(chell_text[2])
+				payload['c_volts'] = float(chell_text[26])
+				payload['c_temp1'] = float(chell_text[27])
+				payload['c_temp2'] = float(chell_text[28])
+				payload['c_lag'] = float(chell_text[29])
+				payload['c_live'] = 1
+			else:
+				payload['c_state'] = 0
+				chell_synced = 0
+				payload['c_volts'] = 0
+				payload['c_temp1'] = 0
+				payload['c_temp2'] = 0
+				payload['c_lag'] = 0
+				payload['c_live'] = 0
+			chell_packet_id_last = chell_packet_counter
+			retries = 99;
+		except:
+			print ("Chell Failed")
 			payload['c_state'] = 0
 			chell_synced = 0
 			payload['c_volts'] = 0
@@ -118,32 +136,26 @@ def upload_data(keyframe):
 			payload['c_temp2'] = 0
 			payload['c_lag'] = 0
 			payload['c_live'] = 0
-		chell_packet_id_last = chell_packet_counter
-	except:
-		print ("Chell Failed")
-		payload['c_state'] = 0
-		chell_synced = 0
-		payload['c_volts'] = 0
-		payload['c_temp1'] = 0
-		payload['c_temp2'] = 0
-		payload['c_lag'] = 0
-		payload['c_live'] = 0
 		
 	if (chell_synced == 1 and gordon_syned == 1):
 		payload['synced'] = 1
 	else:
 		payload['synced'] = 0
 	
-	try:	
-		#determine which gun if any to get image from
-		files = {};
-		if payload['c_state'] < -1:
-			files['img'] = ('i.jpg', session_chell.get(url_chell_image).content)
-				
-		if payload['g_state'] < -1:
-			files['img'] = ('i.jpg', session_gordon.get(url_goron_image).content)
-	except:
-		print("Img get Failed")
+	retries = 0
+	while retries < 2:
+		try:
+			retries = retries + 1
+			#determine which gun if any to get image from
+			files = {};
+			if payload['c_state'] < -1:
+				files['img'] = ('i.jpg', session_chell.get(url_chell_image,timeout=0.01).content)
+					
+			if payload['g_state'] < -1:
+				files['img'] = ('i.jpg', session_gordon.get(url_goron_image,timeout=0.01).content)
+			retries = 99;
+		except:
+			print("Img get Failed")
 		
 	#supress warmup cycle
 	if cycle > 0:
@@ -157,7 +169,42 @@ def upload_data(keyframe):
 		
 	cycle = cycle + 1
 	
-	
+
+ip = ""
+while (ip == ""):
+	fullfile = open('/proc/net/arp', 'r').read()
+	results = re.findall( r'[0-9]+(?:\.[0-9]+){3}', fullfile )
+
+	for item in results:
+		print ("Trying Gordon at " + item)
+		try:
+			requests.get("http://" + item + ":8022/tmp/portal.txt",timeout=0.01)
+		except:
+			print ("Gordon Failed")
+		else:
+			print ("Found Gordon")
+			ip = item
+			break
+			
+		print ("Trying Chell at " + item)
+		try:
+			requests.get("http://" + item + ":8023/tmp/portal.txt",timeout=0.01)
+		except:
+			print ("Chell Failed")
+		else:
+			print ("Found Chell")
+			ip = item
+			break
+			
+	time.sleep(10)
+		
+print ("Building URLs...")		
+url_goron_data ='http://' + ip + ':8022/tmp/portal.txt'
+url_chell_data ='http://' + ip + ':8023/tmp/portal.txt'
+url_goron_image = 'http://' + ip + ':8022/tmp/snapshot.jpg'
+url_chell_image = 'http://' + ip + ':8023/tmp/snapshot.jpg'
+
+
 print("Warming")
 upload_data(0)
 time.sleep(2)
